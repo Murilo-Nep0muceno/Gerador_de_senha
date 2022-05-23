4ef533a99dc6d695201a94fd0da319618135b8d4
Murilo Nepomuceno <murilonep@gmail.com>
2022-05-23 18:35:56 -0300

    inicial

---
+++ index.py
@@ -0,0 +1,29 @@
+from PyQt5 import uic,QtWidgets
+import mysql.connector
+import random
+
+
+def funcao_principal():
+  linha=formulario.lineEdit.text()
+  linha2=formulario.lineEdit_2.text()
+  min="abcdefghijklmnopqrstuvwxyz"
+  mai="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  num="1234567890"
+  symbols="!@#$%&*?\/"
+  use = mai + min + num + symbols
+  password="".join(random.sample(use, int(linha)))
+
+
+  print(linha2, ":" , password)
+
+
+
+
+
+
+app= QtWidgets.QApplication([])
+formulario=uic.loadUi("tela_1.ui")
+formulario.pushButton.clicked.connect(funcao_principal)
+
+formulario.show()
+app.exec()
